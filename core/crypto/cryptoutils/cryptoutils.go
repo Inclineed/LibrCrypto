3055c7a11e6fcdf26bd43cb847d63f42a33e0143
Deepansh Singh <114745264+Inclineed@users.noreply.github.com>
2025-07-21 03:39:22 +0530

    Update cryptoutils.go

--- core/crypto/cryptoutils/cryptoutils.go
+++ core/crypto/cryptoutils/cryptoutils.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/devlup-labs/Libr/core/crypto/config"
+	"github.com/Inclined/LibrCrypto/core/crypto/config"
 )
 
 // GenerateKeyPair generates a new Ed25519 key pair,
